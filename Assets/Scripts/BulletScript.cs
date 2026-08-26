6ebcae15d4a6917ee57a6cfe6a59d065f30d916f
PedroFigAnicesa <edu.arraiol@gmail.com>
2026-08-26 15:25:18 -0300

    Commit pra trocar de sala

---
+++ Assets/Scripts/BulletScript.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerAttack : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void OnAttack(InputValue value)
+    {
+        
+    }
+
+    void Update()
+    {
+        OnAttack();
+    }
+}
